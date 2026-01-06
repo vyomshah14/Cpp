0c67674a2ed578dfcdcf7a2891ed34b3259e80f9
vyomshah14 <vyomshah2021@gmail.com>
2026-01-06 14:46:04 +0530

    Boolean pratice code

---
+++ Day-2/boolean.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   bool a = true;
+   bool b = false;
+
+   cout << " value of a is : " << a << endl;
+   cout << " value of b is : " << b << endl;
+
+    return 0;
+}
