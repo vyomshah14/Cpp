330aee890e18e65475647301bb31d8dbd29ae9e8
vyomshah14 <vyomshah2021@gmail.com>
2026-01-16 13:09:52 +0530

    modified

--- Day-7/OOPS/bank.cpp
+++ Day-7/OOPS/bank.cpp
@@ -1,31 +1,37 @@
+
 #include <iostream>
 using namespace std;
 
 class Bankaccount{
-    private:
-       float AccountNumber;
-       float Password;
-    public:
-       string Name;
-       float Balance;
-       void display(){
-           cout << "Account holder: " << Name << endl;
-           cout << "Account balance: " << Balance << endl;
-       }
-       void setaccountdetails(float accnum, float pass){
-           AccountNumber = accnum;
-           Password = pass;
-       }
-    
-       
+private:
+    long AccountNumber;
+    int Password;
+    // moved to public in original code; keep private but provide public setter if needed
+    void setaccountdetails(long accnum, int pass){
+        AccountNumber = accnum;
+        Password = pass;
+    }
+public:
+    // public wrapper to allow main to set account details
+    void initAccount(long accnum, int pass){
+        setaccountdetails(accnum, pass);
+    }
 
+    float getbalance() const { return Balance; }
+
+    string Name;
+    float Balance;
+    void display() const {
+        cout << "Account holder: " << Name << endl;
+        cout << "Account balance: " << Balance << endl;
+    }
 };
 
 int main(){
     Bankaccount N1;
     N1.Name = "Vyom";
-    N1.Balance = 10000.50;
-    N1.setaccountdetails(12345, 1234);
+    N1.Balance = 10000.50f;
+    N1.initAccount(12345, 1234); // use public initializer
     N1.display();
     return 0;
 }
